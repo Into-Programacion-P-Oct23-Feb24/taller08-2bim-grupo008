d1be0f6e0a4fcff968096dd905ee0b12b87c0204
unknown <sjmayon1@utpl.edu.ec>
2023-12-07 16:34:49 -0500

    problema2

--- problematicas/problema02/src/problema02/Problema02.java
+++ problematicas/problema02/src/problema02/Problema02.java
@@ -15,6 +15,20 @@ public class Problema02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int n1;
+        int n2;
+        int n3;
+
+        for (int i = 25; i >= 1; i--) {
+            
+                n1 = i * 2;
+                n2 = i * 3;
+                n3 = i * 4;
+                System.out.printf("%d-%d-%d-%d\n", i, n1, n2, n3);
+            
+            
+        }
+
     }
-    
+
 }
