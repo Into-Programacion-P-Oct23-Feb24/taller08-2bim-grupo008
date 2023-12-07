c926bdaafdb1a17192bca03932141051515077be
unknown <apadilla2@utpl.edu.ec>
2023-12-07 16:40:42 -0500

    01 03

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -4,6 +4,9 @@
  */
 package problema03;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author LAB.ELECT
@@ -15,6 +18,21 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int limite;
+        double celsius;
+        String cadena ="";
+        
+        limite=20+(20*4);
+        
+        for(int i=20;i<=limite;i+=4){
+            celsius=(5.0/9.0)*(i-32.0);
+            cadena=String.format("%sGrados Fahrenheit: %d a Grados Cel"
+                    + "sius: %.1f\n",cadena,i,celsius);
+        }
+        System.out.printf("%s",cadena);
+        
     }
     
 }
